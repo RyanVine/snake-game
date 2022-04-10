caa4e347ffe5a6472f305f0e7ef48fc808600917
Ryan Vine <R.vine66@gmail.com>
2022-04-10 17:38:34 +0100

    Created file system and began snake game

---
+++ scripts/app.js
@@ -0,0 +1,161 @@
+// create a grid of squares
+// create queryselect of board squares 
+// buttons on screen to move snake
+// global variables for snake
+// interval timer (set interval)
+// array push for the snake body 
+
+const tiles = document.querySelectorAll(".snake-game__grid-tile");
+const buttonLeft = document.querySelector(".snake-game__button--left")
+const buttonRight = document.querySelector(".snake-game__button--right")
+const buttonUp = document.querySelector(".snake-game__button--up")
+const buttonDown = document.querySelector(".snake-game__button--down")
+const buttonPause = document.querySelector(".snake-game__button--pause")
+
+
+console.log(tiles);
+// Global Variables
+// Adding current snake to the center of the board
+let currentSnake = [45, 44, 43];
+let currentSnakeFood = [6];
+// At the start of the game setting the current direction to nothing
+let currentDirection = "";
+
+//Adding the starting class to be visable at the center of board
+tiles[currentSnake[0]].classList.add("snake");
+tiles[currentSnake[1]].classList.add("snake");
+tiles[currentSnake[2]].classList.add("snake");
+tiles[currentSnakeFood[0]].classList.add("food");
+
+// counting every second and updating the location of currrent snake on the board
+// The snake is not moving at the start because it has no direction
+setInterval(() => moveSnake(currentDirection, currentSnake), 200);
+
+
+//make food remove when the snake and food class are on the same div
+setInterval(() => snakeEatFood(currentSnake, currentSnakeFood), 500);
+
+const snakeEatFood = (currentSnakeFood) => {
+
+  if (tiles.classList == "snake" && tiles == "food"){
+    tiles[currentSnakeFood].classList.remove("food");
+  }
+}
+
+
+
+
+
+// Buttons that listens click and updates the current direction
+buttonRight.addEventListener("click", () => {
+  if (currentDirection != "left") {
+ currentDirection = "right";}
+});
+ buttonLeft
+buttonLeft.addEventListener("click", () => {
+  if (currentDirection != "right") {
+  currentDirection = "left"}
+});
+
+buttonUp.addEventListener("click", () => {
+  if (currentDirection != "down") {
+  currentDirection = "up"}
+});
+buttonDown.addEventListener("click", () => {
+  if (currentDirection != "up") {
+  currentDirection = "down"}
+});
+buttonPause.addEventListener("click", () => {
+  currentDirection = "";
+ });
+
+//Detect Key presses
+
+document.addEventListener("keydown", event => {
+  if (event.keyCode === 39 && currentDirection != "left")
+  currentDirection = "right";
+ });
+ document.addEventListener("keydown", event => {
+  if (event.keyCode === 37 && currentDirection != "right")
+  currentDirection = "left";
+ });
+ document.addEventListener("keydown", event => {
+  if (event.keyCode === 38 && currentDirection != "down")
+  currentDirection = "up";
+ });
+ document.addEventListener("keydown", event => {
+  if (event.keyCode === 40 && currentDirection != "up")
+  currentDirection = "down";
+ });
+ document.addEventListener("keydown", event => {
+  if (event.keyCode === 32)
+  currentDirection = "";
+ });
+
+// function to move snake which is called upon every second
+const moveSnake = (direction, snake) => {
+  //Getting first index from snakeHead
+  
+    if (direction === "right") {
+      let snakeHead = snake[0]; 
+    //Checking direction - 
+      let snakeTail = snake[snake.length-1];
+      let newSnakeHead = ++snakeHead;
+      tiles[newSnakeHead].classList.add("snake");
+      tiles[snakeTail].classList.remove("snake");
+      snake.unshift(newSnakeHead);
+      snake.pop();
+      console.log(snake);
+      return;
+  }
+  else if (direction === "left") {
+    let snakeHead = snake[0]; 
+    //Checking direction - 
+      let snakeTail = snake[snake.length-1];
+      let newSnakeHead = --snakeHead;
+      tiles[newSnakeHead].classList.add("snake");
+      tiles[snakeTail].classList.remove("snake");
+      snake.unshift(newSnakeHead);
+      snake.pop();
+      console.log(snake);
+      return;
+  }
+  else if (direction === "up") {
+    let snakeHead = snake[0]; 
+    //Checking direction - 
+      let snakeTail = snake[snake.length-1];
+      let newSnakeHead = snakeHead-10;
+      tiles[newSnakeHead].classList.add("snake");
+      tiles[snakeTail].classList.remove("snake");
+      snake.unshift(newSnakeHead);
+      snake.pop();
+      console.log(snake);
+      return;
+  }
+  else if (direction === "down") {
+    let snakeHead = snake[0]; 
+    //Checking direction - 
+      let snakeTail = snake[snake.length-1];
+      let newSnakeHead = snakeHead+10;
+      tiles[newSnakeHead].classList.add("snake");
+      tiles[snakeTail].classList.remove("snake");
+      snake.unshift(newSnakeHead);
+      snake.pop();
+      console.log(snake);
+      return;
+  }
+  return
+}
+
+
+
+
+
+
+
+
+//!!add a final part to the if statement that updates the snake global variable with the new location from the if statement
+//shift the snakehead++ into the snake array, then use snake.pop
+
+
+// make food push an element onto the array when a div contains both food and snake, then remove the snake class
