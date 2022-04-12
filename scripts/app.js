6a23031b261101741ff334bdd96d683dd8434496
Ryan Vine <R.vine66@gmail.com>
2022-04-12 15:19:34 +0100

    added lose condition when hitting left and right wall

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,4 @@
-// create a grid of squares
-// create queryselect of board squares
-// buttons on screen to move snake
-// global variables for snake
-// interval timer (set interval)
-// array push for the snake body
-
+//Query Selectors
 const tiles = document.querySelectorAll(".snake-game__grid-tile");
 const buttonLeft = document.querySelector(".snake-game__button--left");
 const buttonRight = document.querySelector(".snake-game__button--right");
@@ -12,24 +6,29 @@ const buttonUp = document.querySelector(".snake-game__button--up");
 const buttonDown = document.querySelector(".snake-game__button--down");
 const buttonPause = document.querySelector(".snake-game__button--pause");
 const scoreNum = document.querySelector(".snake-game__score-number");
+
+
 // Global Variables
 // Adding current snake to the center of the board
 let currentSnake = [45];
+// Adds food to the board
 let currentFood = 6;
+// Preps the score
 let currentScore = scoreNum.innerhtml;
-// At the start of the game setting the current direction to nothing
+// At the start of the game setting the current direction to nothing so no movement occurs
 let currentDirection = "";
 
-//Adding the starting class to be visable at the center of board
+// Adding the starting class to be visable at the center of board
 tiles[currentSnake[0]].classList.add("snake");
 tiles[currentFood].classList.add("food");
 
-// counting every second and updating the location of currrent snake on the board
-// The snake is not moving at the start because it has no directio
 
+// counting every second and updating the location of currrent snake on the board
+// The snake is not moving at the start because it has no direction
 currentSpeed = 250;
 const interval = setInterval(() => moveSnake(currentDirection, currentSnake), currentSpeed); 
 
+//Win condition to check if the first index of the snake array is the same as the food
 const isWin = (snakeArray, food) => {
   if (snakeArray[0] == food) {
     return true;
@@ -38,6 +37,29 @@ const isWin = (snakeArray, food) => {
 };
 
 
+
+//lose condition
+const isLose = (snakeArray) => {
+  tiles[snakeArray[0]].classList.contains("death_tile-right")
+  if (tiles[snakeArray[0]].classList.contains("death_tile-right") && tiles[snakeArray[1]].classList.contains("death_tile-left")){ 
+    console.log("You lose!")
+    return true;
+
+  } else if (tiles[snakeArray[0]].classList.contains("death_tile-left") && tiles[snakeArray[1]].classList.contains("death_tile-right")){
+    return true;
+  }
+  return false;
+};
+
+  //lose condition
+  const loseCon = (snakeArray) => {
+    alert("Lol get rekt scrub")
+    if (isLose(snakeArray) == true) {
+      ;
+    }
+  }
+ 
+
 //random integer finder
 const randomTileNumber = (min, max) => {
   min = Math.ceil(min);
@@ -45,9 +67,9 @@ const randomTileNumber = (min, max) => {
   return Math.floor(Math.random() * (max - min) + min);
 };
 
-// make function with currentSnake[0] == currentFood
 
-// Buttons that listens click and updates the current direction
+
+// Buttons that listen for a click and updates the current direction
 buttonRight.addEventListener("click", () => {
   if (currentDirection != "left") {
     currentDirection = "right";
@@ -74,8 +96,7 @@ buttonPause.addEventListener("click", () => {
   currentDirection = "";
 });
 
-//Detect Key presses
-
+//Detect Key presses for the above
 document.addEventListener("keydown", (event) => {
   if (event.keyCode === 39 && currentDirection != "left")
     currentDirection = "right";
@@ -101,7 +122,7 @@ const moveSnake = (direction, snake) => {
   //Getting first index from snakeHead
   const isCurrentWin = isWin(currentSnake, currentFood);
   console.log(isCurrentWin);
-  //!!food eating section
+  // Checks for win condition, if true then it moves the food to a random location, adds a piece to the snake and updates the score
   const winCon = () => {
     if (isCurrentWin == true) {
       tiles[currentFood].classList.remove("food");
@@ -109,32 +130,44 @@ const moveSnake = (direction, snake) => {
       tiles[currentFood].classList.add("food");
       currentSnake.push(currentFood);
       scoreNum.innerHTML = parseInt(scoreNum.innerHTML) + 1;
-      console.log(currentSpeed);
     }
   };
 
+
+
+      // Movement direction is checked, and then functions are ran
+      //right Movement
   if (direction === "right") {
     const isCurrentWin = isWin(currentSnake, currentFood);
     if (isCurrentWin) {
       winCon();
+    } else{
+    let isCurrentLose = isLose(currentSnake)
+    if (isCurrentLose) {
+      loseCon()
+    }
     }
     let snakeHead = snake[0];
-    //Checking direction -
     let snakeTail = snake[snake.length - 1];
     let newSnakeHead = ++snakeHead;
     tiles[newSnakeHead].classList.add("snake");
     tiles[snakeTail].classList.remove("snake");
     snake.unshift(newSnakeHead);
     snake.pop();
-    console.log(snake);
     return;
+    
 
-    //left movement
+    //left Movement
   } else if (direction === "left") {
     const isCurrentWin = isWin(currentSnake, currentFood);
     if (isCurrentWin) {
       winCon();
-    }
+    } else{
+      let isCurrentLose = isLose(currentSnake)
+      if (isCurrentLose) {
+        loseCon()
+      }
+    }  
     let snakeHead = snake[0];
     let snakeTail = snake[snake.length - 1];
     let newSnakeHead = --snakeHead;
@@ -142,8 +175,8 @@ const moveSnake = (direction, snake) => {
     tiles[snakeTail].classList.remove("snake");
     snake.unshift(newSnakeHead);
     snake.pop();
-    console.log(snake);
     return;
+    // Up movement
   } else if (direction === "up") {
     const isCurrentWin = isWin(currentSnake, currentFood);
     if (isCurrentWin) {
@@ -156,22 +189,20 @@ const moveSnake = (direction, snake) => {
     tiles[snakeTail].classList.remove("snake");
     snake.unshift(newSnakeHead);
     snake.pop();
-    console.log(snake);
     return;
+    // Down movement
   } else if (direction === "down") {
     const isCurrentWin = isWin(currentSnake, currentFood);
     if (isCurrentWin) {
       winCon();
     }
     let snakeHead = snake[0];
-    //Checking direction -
     let snakeTail = snake[snake.length - 1];
     let newSnakeHead = snakeHead + 20;
     tiles[newSnakeHead].classList.add("snake");
     tiles[snakeTail].classList.remove("snake");
     snake.unshift(newSnakeHead);
     snake.pop();
-    console.log(snake);
     return;
   }
   return;
@@ -179,17 +210,7 @@ const moveSnake = (direction, snake) => {
 
 //!!              Things to add:
 
-/* . Food appears on random tile when eaten by snake
-   . Snake increases in size when food is eaten
-   . Score increases when food is eaten
-   . Game Over pops up when touching the floor or ceiling
-   . Game Over pops up when the snake touches itself
-   . Increase size of grid
-   . Difficulty increase/decrease button that increases setInterval
-   ?? Ask about hitting edges
-   ?? Ask about eating food
-   !!tiles[currentSnake[currentSnake.length -1]].classList.add("snake"); + class list + array push
-    !!if currentSnake & Currentsnake +1 == class death then game over
-    !!add coments so I understand and can explain the code  
-    !currentspeed  !=
+/*  Lose condition if current tile and next tile is a death tile, and vice versa
+    Lose condition if  snake hits itself
+    decrease set interval when wincon is true until 150
 */
